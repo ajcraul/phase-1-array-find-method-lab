050506e70409296b211519e5fec98d79613a8755
allison craul <craul.aj@gmail.com>
2023-03-22 14:20:12 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+/* code your solution here
+function superbowlWin(arrayObject) {
+    const record = [{
+        year: "2015",
+        result: "W"
+    },
+    {
+        year: "2014",
+        result: "N/a"
+    },
+    {
+        year: "2013",
+        result: "L"
+    },
+    ]   
+}
+
+array.prototype.find(result); */
+
+superbowlWin = (record) => {
+    const result = record.find( record => record.result === "W" );
+    return !!result ? result.year : undefined;
+}
